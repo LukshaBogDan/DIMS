a1d47c60df865de6e0a3af0802f71e19d4006e1e
Bogdan <bogdanluksha@gmail.com>
2019-08-06 23:50:37 +0300

    Add Progress views

---
+++ DIMS/DIMS.Server/Models/ProgressViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HIMS.Server.Models
+{
+    public class ProgressViewModel
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string TaskName { get; set; }
+        public string TrackNote { get; set; }
+        public DateTime TrackDate { get; set; }
+    }
+}
