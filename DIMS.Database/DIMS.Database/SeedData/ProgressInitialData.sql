b7f65149a335d32323014f477d0fa99cba74e693
Bogdan <bogdanluksha@gmail.com>
2019-08-06 23:52:55 +0300

    Add Progress seed

---
+++ DIMS.Database/DIMS.Database/SeedData/ProgressInitialData.sql
@@ -0,0 +1,5 @@
+﻿INSERT INTO [dbo].[Progress]
+		([UserId], [UserName], [TaskName], [TrackNote], [TrackDate])
+	VALUES
+		(1, 'alex', 'CreateDB', 'Create user table', '10-11-2018'),
+		(1, 'alex', 'CreateDB', 'Create user view', '10-11-2018')
---
+++ DIMS.Database/DIMS.Database/Tables/Progress.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [dbo].[Progress]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [UserId] INT NOT NULL, 
+    [UserName] NCHAR(10) NULL, 
+    [TaskName] NCHAR(10) NULL, 
+    [TrackNote] NCHAR(10) NULL, 
+    [TrackDate] DATE NULL
+)
