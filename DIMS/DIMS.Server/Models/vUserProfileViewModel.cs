39bde8e1f325d3802b529b46dea177e08b5a7c8d
Bogdan <bogdanluksha@gmail.com>
2019-08-06 23:57:43 +0300

    Add validation for models

--- DIMS/DIMS.Server/Models/vUserProfileViewModel.cs
+++ DIMS/DIMS.Server/Models/vUserProfileViewModel.cs
@@ -9,26 +9,20 @@ namespace HIMS.Server.Models
     public class vUserProfileViewModel
     {
         public int UserId { get; set; }
-
-        [Required]
-        [Display(Name = "Name")]
         public string FullName { get; set; }
-        [EmailAddress]
-        [Required]
-        [Display(Name="Mail")]
         public string Email { get; set; }
         public string Direction { get; set; }
         public string Sex { get; set; }
         public string Education { get; set; }
         public int Age { get; set; }
-        [Range(1,10)]
         public double UniversityAverageScore { get; set; }
-        [Range(1, 10)]
         public double MathScore { get; set; }
         public string Address { get; set; }
-        [Phone]
         public string MobilePhone { get; set; }
         public string Skype { get; set; }
+        [Required]
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd.MM.yyyy}")]
         public DateTime StartDate { get; set; }
     }
 }
